2103450c0aefb76cb4037212783c67c8b0dc63e2
bony <rudrabandhu@gmail.com>
2020-07-04 10:35:26 -0700

    minor bug fixes

--- code/train_tweet_NN.py
+++ code/train_tweet_NN.py
@@ -21,7 +21,6 @@ import torch.optim as optim
 import torch.optim.lr_scheduler as sched
 import torch.utils.data as data
 
-#from transformers.data.processors.squad import SquadResult, SquadV2Processor
 from transformers.data.metrics.squad_metrics import (
     compute_predictions_log_probs, compute_predictions_logits, squad_evaluate,
 )
@@ -35,8 +34,6 @@ import tweetrecords
 import metrics
 import tf_idf
 
-#utils.batch_tokens_to_matrix(tokens_batch, vocabulary)
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--engage_mode', help='tweet: 0, retweet: 1, retweet with comment: 2, like:3', default='retweet')
 parser.add_argument('--batch_size', help='batch size', type=int, default=1000)
@@ -44,23 +41,33 @@ parser.add_argument('--batch_size', help='batch size', type=int, default=1000)
 #train_first_1000
 trainf = 'train_retweet_min_19.tsv'
 valf = 'val_retweet_min_19.tsv'
+parser.add_argument('--model', help = 'Model type: MLP or NCF', default = 'MLP')
 parser.add_argument('--mode', help = 'train or eval: train_eval or eval', default = 'train_eval')
+
+model_name_MLP = 'MLP'
+model_name_NCF = 'NCF_MF_MLP'
+parser.add_argument('--model_name', help = 'change the model name as needed', default = model_name_MLP)
+MLP_pretrained = 'MLP_R4.pt'
+NCF_pretrained = "NCF_MF_MLP.pt"
+parser.add_argument('--pretrained_model', help = 'filename of pretrained model',  default = MLP_pretrained)
+
 parser.add_argument('--vocab_file', help='vocabulary term document count file', default = 'document_freq_count_train.pickle')
 parser.add_argument('--train_file', help = 'name of train file', default = trainf)
 parser.add_argument('--eval_file', help = 'name of validation file', default = valf)
-parser.add_argument('--model_save_file', help = 'name of file to save trained model: train_MLP_Baseline, train_NCF_MLP', default = "../output/train_MLP_NCF.pickle")
+#parser.add_argument('--model_save_file', help = 'name of file to save trained model: train_MLP_Baseline, train_NCF_MLP', default = "../output/train_MLP_NCF.pickle")
 
 parser.add_argument('--data_dir', help="data directory", default='../../s3mnt/data')
 parser.add_argument('--output_dir', help = 'output data directory', default = "../output")
 parser.add_argument('--saved_model_dir', help = 'directory to save trained models', default = "../saved_models")
 parser.add_argument('--log_dir', help = 'output data directory', default = "../logs")
 
+
 parser.add_argument('--log_every', help = 'log data every this number of epochss', default = 1)
 parser.add_argument('--eval_every', help = 'log data every this number of batches', default = 2)
-parser.add_argument('--train_epochs', help = 'Train this many number of epochs', default = 50)
+parser.add_argument('--train_epochs', help = 'Train this many number of epochs', default = 30)
 
 parser.add_argument('--optimizer', help = 'NN Optmizer: choices: adam, rmsprop, sgd', default = 'adam')
-parser.add_argument('--adam_lr', help = 'learning rate: adam', default = 0.001)
+parser.add_argument('--adam_lr', help = 'learning rate: adam', default = 0.0001)
 parser.add_argument('--l2_regularization', help = 'l2_regularization parameter', default = 0.000001)
 parser.add_argument('--sgd_lr', help = 'learning rate: SGD', default = 0.0001)
 parser.add_argument('--sgd_momentum', help = 'momentum in SGD', default = 0.9)
@@ -88,13 +95,14 @@ def main():
         print("loggiing directory exists")
 
     #log_file = 'train_MLP_Baseline_round3_' + str(engage_mode) + '.log'
-    log_file = 'train_NCF_MLP_round3' + str(engage_mode) + '.log'
+    log_file = args.model_name +'_' + str(engage_mode) + '.log'
 
     utils.set_logger(os.path.join(logging_dir, log_file))
-    #logging.info('model = Multi layer NN with BERT embeddings and other features as input, engagement mode: Retweet')
-    logging.info('model  = Neural Collaborative Filtering, engagement, Optimizer: Adam, mode: Retweet')
+    logging.info('model = Multi layer NN with BERT embeddings and other features as input, engagement mode: Retweet')
+    logging.info('Does not use UserID embeddings')
+    #logging.info('model  = Neural Collaborative Filtering, engagement, Optimizer: Adam, mode: Retweet')
     logging.info('Using BERT embeddings values of tweet tokens and one hot encoding of other features')
-    logging.info('Uses  user id embeddings')
+    #logging.info('Uses  user id embeddings')
     logging.info('training set: '.format(args.train_file))
     #logging.info('args parameters: %r ', args)
     for arg, value in sorted(vars(args).items()):
@@ -109,22 +117,28 @@ def main():
 
     #Instantiating Val tweet records
     TR_val = tweetrecords.TweetRecords(val_filepath, batch_size, embed_tokens_next=True)
-    print("loading unique_id_token embeddings dict")
-    fhandle = bz2.BZ2File(os.path.join(args.data_dir, 'val_retweet_min_19_token_embeds_mean.pickle'), 'r')
-    #fhandle = bz2.BZ2File('../../s3mnt/data/val_retweet_min_19_token_embeds_mean.pickle', 'r')
+    logging.info("loading unique_id_token embeddings dict")
+    fhandle = bz2.BZ2File(os.path.join(data_dir, 'val_retweet_min_19_token_embeds_mean.pickle', 'r'))
     TR_val.unique_id_tokenembedsdict = pickle.load(fhandle)
     fhandle.close()
 
-    #engine = MLP_Baseline.MLP_BaselineEngine(args)
-    engine = NCF_MLP.NCF_Engine(args)
+    rng = np.random.RandomState(2020)
+   
+    if args.model == 'MLP':
+        engine = MLP_Baseline.MLP_BaselineEngine(args)
+        
+    elif args.model == 'NCF':
+        engine = NCF_MLP.NCF_Engine(args)
+        args.model_name = model_name_NCF
+        args.pretrained_model = NCF_pretrained
+
     logging.info('Starting Training')
 
     if args.mode == 'train_eval':
         #Instantiating Train tweet records
         TR_train = tweetrecords.TweetRecords(train_filepath, batch_size, embed_tokens_next=True)
         print("loading unique_id_token embeddings dict")
-        fhandle = bz2.BZ2File(os.path.join(args.data_dir, 'train_retweet_min_19_token_embeds_mean.pickle'), 'r')
-        #fhandle = bz2.BZ2File('../../s3mnt/data/train_retweet_min_19_token_embeds_mean.pickle', 'r')
+        fhandle = bz2.BZ2File(os.path.join(data_dir, 'train_retweet_min_19_token_embeds_mean.pickle', 'r'))
         TR_train.unique_id_tokenembedsdict = pickle.load(fhandle)
         fhandle.close()
 
@@ -140,12 +154,15 @@ def main():
                 if (epoch % args.eval_every ==0):
                     engine.save_model(args, epoch)
                     engine.train_an_epoch(TR_val, args, epoch, mode='eval')
+        TR_train.close_file()
     
         logging.info('Finished Training')
     elif args.mode == 'eval':
-        engine.train_an_epoch(TR_train, args, epoch, mode='eval')
+        saved_model_path = os.path.join(args.saved_model_dir  ,  args.pretrained_model)
+        engine.load_model(saved_model_path)
+        engine.train_an_epoch(TR_val, args, 0, mode='eval')
     
-    TR_train.close_file()
+    
     TR_val.close_file()
 
 if __name__ == "__main__":
